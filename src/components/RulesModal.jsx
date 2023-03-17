66a59f8d45b3dd7830bd01cebb11084980fc5383
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-03-16 20:02:17 -0600

    start with modal

--- src/components/RulesModal.jsx
+++ src/components/RulesModal.jsx
@@ -1,6 +1,9 @@
 
-export const RulesModal = () => {
+export const RulesModal = ({ isOpen, setIsOpen }) => {
   return (
-    <div>RulesModal</div>
+    <>
+      <div class='bg-[rgba(0,_0,_0,_0.2)] w-screen h-screen z-0 top-1/2 left-1/2 translate-y-1/2 translate-x-1/2' onClick={() => setIsOpen(false)} />
+      <div />
+    </>
   );
 };
