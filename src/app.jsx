eb24630f0c87db95226dcb753f8da1239e533946
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-03-17 01:14:07 -0600

    modal done

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,23 @@
 import logo from './assets/logo.svg';
+import { useState } from 'preact/hooks';
 import { Score } from './components/Score';
-import { RulesModal } from './components/RulesModal';
+import RulesModal from './components/RulesModal';
 import { GameBoard } from './components/GameBoard';
 
 export function App () {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <div class='flex flex-col items-center min-h-screen p-8'>
+      <div class='flex flex-col items-center min-h-screen max-h-fit p-8'>
         <header class='w-1/2 px-6 py-4 flex place-content-evenly border-2 rounded-xl border-headeer-outline'>
           <img class='mr-auto' src={logo} alt='Rock, Paper, Scissors' />
           <Score />
         </header>
         <GameBoard />
-        <button class='self-end mt-auto border-2 rounded-xl border-headeer-outline py-2 px-10 hover:border-white hover:bg-white hover:text-black mix-blend-screen'>RULES</button>
-        <RulesModal />
+        <button class='self-end mt-auto border-2 rounded-xl border-headeer-outline py-2 px-10 hover:border-white hover:bg-white hover:text-black mix-blend-screen' onClick={() => setOpen(true)}>RULES</button>
+        <div class={`${open ? 'visible' : 'transition-all hidden'} transition-all`}>
+          <RulesModal setIsOpen={setOpen} />
+        </div>
       </div>
 
     </>
--- src/components/RulesModal.jsx
+++ src/components/RulesModal.jsx
@@ -1,9 +1,23 @@
+import close from '../assets/icon-close.svg';
+import rules from '../assets/image-rules.svg';
 
-export const RulesModal = ({ isOpen, setIsOpen }) => {
+const RulesModal = ({ setIsOpen }) => {
   return (
     <>
-      <div class='bg-[rgba(0,_0,_0,_0.2)] w-screen h-screen z-0 top-1/2 left-1/2 translate-y-1/2 translate-x-1/2' onClick={() => setIsOpen(false)} />
-      <div />
+      <div class='bg-[rgba(0,_0,_0,_0.3)] w-full min-h-screen z-0 fixed top-0 left-0' onClick={() => setIsOpen(false)} />
+      <div class='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <div class='min-w-fit min-h-fit bg-white z-10 rounded-2xl py-6 px-8'>
+          <div class='flex justify-between mb-12'>
+            <h2 class='text-dark-text text-3xl font-bold'>RULES</h2>
+            <button onClick={() => setIsOpen(false)}>
+              <img src={close} alt='close' />
+            </button>
+          </div>
+          <img class='my-4 mx-8' src={rules} alt='rules' />
+        </div>
+      </div>
     </>
   );
 };
+
+export default RulesModal;
